32ee03f0d027507e738c7dd1effa6798d2194e71
restby <restby@gmail.com>
2023-02-23 08:06:18 +0300

    features

--- src/js/components/tooltip.js
+++ src/js/components/tooltip.js
@@ -2,5 +2,5 @@ import { createPopper, right } from '@popperjs/core';
 const el = document.querySelector('.tooltip__btn');
 const tooltip = document.querySelector('.tooltip__txt');
 createPopper(el, tooltip, {
-  placement: 'right-start'
+  placement: 'top-start'
 });
