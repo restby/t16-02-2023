aef02d6fefd22e91e8d2e33f310aec1b09fb4e04
restby <restby@gmail.com>
2023-03-02 08:08:58 +0300

    slider next

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -2,4 +2,9 @@ import Swiper, { Navigation, Scrollbar } from 'swiper';
 Swiper.use([Navigation, Scrollbar]);
 const swiper = new Swiper('.trainers__content', {
   slidesPerView: 'auto',
+  spaceBetween: 40,
+  scrollbar: {
+    el: '.trainers__scroll',
+    draggable: true,
+  },
 });
