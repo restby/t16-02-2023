492d121b19fe8799f1a98704a05c6527ab72dc49
restby <restby@gmail.com>
2023-03-13 07:55:54 +0300

    footer,hero,form

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import "../js/components/smooth-scroll";
 import "../js/components/tooltip";
 import "../js/components/modals";
 import "../js/components/slider";
+import "../js/components/validation";
---
+++ src/js/components/validation.js
@@ -0,0 +1,51 @@
+import { validateForms } from '../functions/validate-forms';
+
+const rules = [
+  {
+    ruleSelector: '.input-name',
+    rules: [
+      {
+        rule: 'minLength',
+        value: 3
+      },
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните имя!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните телефон!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-email',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните e-mail!'
+      },
+      {
+        rule: 'email',
+        value: true,
+        errorMessage: 'Введите корректный e-mail!'
+      }
+    ]
+  }
+];
+
+const afterForm = () => {
+  console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.order__form', rules, afterForm);
